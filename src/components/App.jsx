415f32d6884b8f4b14eaf5616cb82bd9d28912f9
Nicolás M. Pardo <nicolasm@nikodermus.media>
2020-02-27 23:28:19 -0500

    fix(style): include stylus template and classes
    Create what's needed for the workshop in terms of styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import Github from '../svg/Github';
 
-const App = () => <h1 >React for everyone isasdas it?</h1>;
+const App = () => <>
+
+    <a href="" className="social-link"><Github /></a>
+    <figure className="round">
+        <video className="round__inner" autoPlay muted>
+            <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4" />
+            <source src="https://www.w3schools.com/html/mov_bbb.ogg" type="video/ogg" />
+            Your browser does not support HTML5 video.
+        </video>
+    </figure>
+    <figure className="square square--small">
+        <img className="square__img" src="https://source.unsplash.com/random/800x700" alt="" />
+    </figure>
+    <figure className="square square--large">
+        <img className="square__img" src="https://source.unsplash.com/random/800x700?a=1" alt="" />
+    </figure>
+    <span className="tooltip">¿Qué personaje historico eres?</span>
+    <h1>React for everyone isasdas it?</h1></>;
 
 export default App;
---
+++ src/svg/Facebook.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Github = () => {
+    return (<svg xmlns="http://www.w3.org/2000/svg" width="13.46" height="25.131" viewBox="0 0 13.46 25.131">
+        <path id="Icon_awesome-facebook-f" data-name="Icon awesome-facebook-f" d="M14.187,14.136l.7-4.548H10.521V6.637a2.274,2.274,0,0,1,2.564-2.457h1.984V.307A24.194,24.194,0,0,0,11.547,0C7.954,0,5.6,2.178,5.6,6.122V9.588H1.609v4.548H5.6V25.131h4.917V14.136Z" transform="translate(-1.609)" fill="#a5b2b5" />
+    </svg>
+    );
+};
+
+export default Github;
