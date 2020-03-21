edb19df8baba6a25def7fbae466afe36d49ac9f6
Nicolás M. Pardo <nicolasm@nikodermus.media>
2020-03-21 13:45:38 -0500

    feat(src): base

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,6 @@
-/*
-    This is where our React App will live,
-    and where we will be working
-*/
-
 // This JSON will be our "back-end" (data)
 const API_URL =
-    'https://gist.githubusercontent.com/Nikodermus/8e12541f8cf2ad17155169e638cd193f/raw/8bbbacc31f4b9c1a10f37725f30a57b208e28307/face-filters.json';
+    'https://gist.githubusercontent.com/Nikodermus/b80cd38c2314414a22d6588f735748fa/raw/cd43c2054f0ba351cdeb20417ec6e790e8146a93/paranormal-library.json';
 
 const App = () => 'We are ready to React!';
 
--- src/random.js
+++
@@ -1,8 +0,0 @@
-/*
-    Small function to generate a random number
-    between a given range (Integers)
-*/
-const randomInRange = (min = 0, max = 10) =>
-    Math.floor(Math.random() * (max - min + 1) + min);
-
-export default randomInRange;
