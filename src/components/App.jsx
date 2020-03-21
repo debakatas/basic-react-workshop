a80de743304263dacf4b68f06c0a130a808c5735
Nicolás M. Pardo <nicolasm@nikodermus.media>
2020-03-21 14:16:39 -0500

    Update live workshop

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,15 @@
 // This JSON will be our "back-end" (data)
+import React from 'react';
+import Book from './Book';
+
 const API_URL =
     'https://gist.githubusercontent.com/Nikodermus/b80cd38c2314414a22d6588f735748fa/raw/cd43c2054f0ba351cdeb20417ec6e790e8146a93/paranormal-library.json';
 
-const App = () => 'We are ready to React!';
+const App = () => (
+    <div>
+        <Book></Book>
+        <p>Biblioteca Paranormal</p>
+    </div>
+);
 
 export default App;
---
+++ src/components/Book.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Book = () => (
+    <div className="book">
+        <figure className="book__img">
+            <img
+                src="http://debakatas.com/cover/manuscrito-voynich.jpg"
+                alt=""
+            />
+        </figure>
+        <button type="button">Pedir</button>
+    </div>
+);
+
+export default Book;
